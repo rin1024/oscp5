8873a87e5fb552756bab67ec7c41caea59f3614c
rin1024 <sorauta.net@gmail.com>
2024-11-28 21:29:25 +0800

    add comment

--- src/main/java/netP5/Bytes.java
+++ src/main/java/netP5/Bytes.java
@@ -1,19 +1,16 @@
 /**
- * A network library for processing which supports UDP, TCP and Multicast.
+ * UDP、TCP、およびマルチキャストをサポートするProcessing用のネットワークライブラリ。
  *
  * <p>##copyright##
  *
- * <p>This library is free software; you can redistribute it and/or modify it under the terms of the
- * GNU Lesser General Public License as published by the Free Software Foundation; either version
- * 2.1 of the License, or (at your option) any later version.
+ * <p>このライブラリはフリーソフトウェアです；あなたはGNU Lesser General Public Licenseの条件に従って、
+ * このライブラリを再配布または修正できます；ライセンスのバージョン2.1、または（あなたの選択で）それ以降のバージョンに基づくものです。
  *
- * <p>This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Lesser General Public License for more details.
+ * <p>このライブラリは、役立つことを期待して配布されていますが、商業性や特定の目的への適合性など、いかなる保証もありません； 詳細についてはGNU Lesser General Public
+ * Licenseをご覧ください。
  *
- * <p>You should have received a copy of the GNU Lesser General Public License along with this
- * library; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- * MA 02111-1307 USA
+ * <p>GNU Lesser General Public Licenseのコピーをこのライブラリと一緒に受け取ったはずです； もし受け取っていない場合は、Free Software
+ * Foundation, Inc.に書き送ってください。 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  *
  * @author ##author##
  * @modified ##date##
@@ -31,7 +28,7 @@ public class Bytes {
 
   private Bytes() {}
 
-  /** converts an object array into a String that is formated like a list */
+  /** オブジェクト配列をリスト形式でフォーマットされた文字列に変換します */
   public static String getAsString(Object[] theObject) {
     StringBuffer s = new StringBuffer();
     for (int i = 0; i < theObject.length; i++) {
@@ -40,6 +37,7 @@ public class Bytes {
     return s.toString();
   }
 
+  /** バイト配列を文字列に変換します */
   public static String getAsString(byte[] theBytes) {
     StringBuffer s = new StringBuffer();
     for (int i = 0; i < theBytes.length; i++) {
@@ -48,6 +46,7 @@ public class Bytes {
     return s.toString();
   }
 
+  /** バイト配列を整数（int）に変換します */
   public static int toInt(byte abyte0[]) {
     return (abyte0[3] & 0xff)
         + ((abyte0[2] & 0xff) << 8)
@@ -55,6 +54,7 @@ public class Bytes {
         + ((abyte0[0] & 0xff) << 24);
   }
 
+  /** バイト配列を長整数（long）に変換します */
   public static long toLong(byte abyte0[]) {
     return ((long) abyte0[7] & 255L)
         + (((long) abyte0[6] & 255L) << 8)
@@ -66,20 +66,24 @@ public class Bytes {
         + (((long) abyte0[0] & 255L) << 56);
   }
 
+  /** バイト配列を浮動小数点数（float）に変換します */
   public static float toFloat(byte abyte0[]) {
     int i = toInt(abyte0);
     return Float.intBitsToFloat(i);
   }
 
+  /** バイト配列を倍精度浮動小数点数（double）に変換します */
   public static double toDouble(byte abyte0[]) {
     long l = toLong(abyte0);
     return Double.longBitsToDouble(l);
   }
 
+  /** 整数をバイト配列に変換します（デフォルトは4バイト） */
   public static byte[] toBytes(int i) {
     return toBytes(i, new byte[4]);
   }
 
+  /** 整数をバイト配列に変換します（指定されたバイト配列に変換） */
   public static byte[] toBytes(int i, byte abyte0[]) {
     abyte0[3] = (byte) i;
     i >>>= 8;
@@ -91,10 +95,12 @@ public class Bytes {
     return abyte0;
   }
 
+  /** 長整数をバイト配列に変換します（デフォルトは8バイト） */
   public static byte[] toBytes(long l) {
     return toBytes(l, new byte[8]);
   }
 
+  /** 長整数をバイト配列に変換します（指定されたバイト配列に変換） */
   public static byte[] toBytes(long l, byte abyte0[]) {
     abyte0[7] = (byte) (int) l;
     l >>>= 8;
@@ -114,6 +120,7 @@ public class Bytes {
     return abyte0;
   }
 
+  /** 2つのバイト配列が等しいかどうかを比較します */
   public static boolean areEqual(byte abyte0[], byte abyte1[]) {
     int i = abyte0.length;
     if (i != abyte1.length) {
@@ -124,10 +131,10 @@ public class Bytes {
         return false;
       }
     }
-
     return true;
   }
 
+  /** 2つのバイト配列を結合します */
   public static byte[] append(byte abyte0[], byte abyte1[]) {
     byte abyte2[] = new byte[abyte0.length + abyte1.length];
     System.arraycopy(abyte0, 0, abyte2, 0, abyte0.length);
@@ -135,6 +142,7 @@ public class Bytes {
     return abyte2;
   }
 
+  /** 3つのバイト配列を結合します */
   public static byte[] append(byte abyte0[], byte abyte1[], byte abyte2[]) {
     byte abyte3[] = new byte[abyte0.length + abyte1.length + abyte2.length];
     System.arraycopy(abyte0, 0, abyte3, 0, abyte0.length);
@@ -143,10 +151,12 @@ public class Bytes {
     return abyte3;
   }
 
+  /** バイト配列の一部をコピーします */
   public static byte[] copy(byte abyte0[], int i) {
     return copy(abyte0, i, abyte0.length - i);
   }
 
+  /** バイト配列の指定した範囲をコピーします */
   public static byte[] copy(byte abyte0[], int i, int j)
       throws java.lang.ArrayIndexOutOfBoundsException {
     byte abyte1[] = new byte[j];
@@ -154,180 +164,203 @@ public class Bytes {
     return abyte1;
   }
 
+  /** バイト配列をマージします */
   public static void merge(byte abyte0[], byte abyte1[], int i, int j, int k) {
     System.arraycopy(abyte0, i, abyte1, j, k);
   }
 
+  /** バイト配列を指定位置にマージします */
   public static void merge(byte abyte0[], byte abyte1[], int i) {
     System.arraycopy(abyte0, 0, abyte1, i, abyte0.length);
   }
 
+  /** バイト配列をマージします */
   public static void merge(byte abyte0[], byte abyte1[]) {
     System.arraycopy(abyte0, 0, abyte1, 0, abyte0.length);
   }
 
+  /** バイト配列の一部をマージします */
   public static void merge(byte abyte0[], byte abyte1[], int i, int j) {
     System.arraycopy(abyte0, 0, abyte1, i, j);
   }
 
+  // バイト配列の指定範囲を16進数の文字列に変換する
   public static String toString(byte abyte0[], int i, int j) {
-    char ac[] = new char[j * 2];
+    char ac[] = new char[j * 2]; // 16進数の2桁ずつで1バイトを表現するため、サイズを設定
     int k = i;
     int l = 0;
+    // 指定された範囲でバイト配列を16進数に変換
     for (; k < i + j; k++) {
       byte byte0 = abyte0[k];
-      ac[l++] = hexDigits[byte0 >>> 4 & 0xf];
-      ac[l++] = hexDigits[byte0 & 0xf];
+      ac[l++] = hexDigits[byte0 >>> 4 & 0xf]; // 高位4ビットを16進数に変換
+      ac[l++] = hexDigits[byte0 & 0xf]; // 下位4ビットを16進数に変換
     }
 
-    return new String(ac);
+    return new String(ac); // 結果を文字列として返す
   }
 
+  // バイト配列全体を16進数の文字列に変換する
   public static String toString(byte abyte0[]) {
-    return toString(abyte0, 0, abyte0.length);
+    return toString(abyte0, 0, abyte0.length); // 全体を変換
   }
 
+  // バイト配列の内容を16進数と文字で出力する
   public static void printBytes(byte[] byteArray) {
+    // 配列の各バイトを16進数と対応する文字で表示
     for (int i = 0; i < byteArray.length; i++) {
       System.out.print(
-          (char) byteArray[i]
+          (char) byteArray[i] // 文字として表示
               + " ("
-              + hexDigits[byteArray[i] >>> 4 & 0xf]
+              + hexDigits[byteArray[i] >>> 4 & 0xf] // 高位4ビットの16進数表示
               + ""
-              + hexDigits[byteArray[i] & 0xf]
+              + hexDigits[byteArray[i] & 0xf] // 下位4ビットの16進数表示
               + ")  ");
-      if ((i + 1) % 4 == 0) {
+      if ((i + 1) % 4 == 0) { // 4バイトごとに改行
         System.out.print("\n");
       }
     }
   }
 
+  // 16進数の文字配列。各バイトの16進数表現をここから取得
   public static final char hexDigits[] = {
     '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'
   };
 
-  /** ByteStream */
+  // 整数をリトルエンディアン形式の4バイト配列に変換
   private static byte[] toByteArray(int in_int) {
     byte a[] = new byte[4];
+    // 整数の各バイトを抽出して配列に格納
     for (int i = 0; i < 4; i++) {
-
       int b_int = (in_int >> (i * 8)) & 255;
       byte b = (byte) (b_int);
-
       a[i] = b;
     }
-    return a;
+    return a; // 配列を返す
   }
 
+  // 整数をビッグエンディアン形式の4バイト配列に変換
   private static byte[] toByteArrayBigEndian(int theInt) {
     byte a[] = new byte[4];
+    // 整数をビッグエンディアン形式で配列に格納
     for (int i = 0; i < 4; i++) {
       int b_int = (theInt >> (i * 8)) & 255;
       byte b = (byte) (b_int);
-      a[3 - i] = b;
+      a[3 - i] = b; // 配列の先頭に格納
     }
-    return a;
+    return a; // 配列を返す
   }
 
+  // 4バイトのバイト配列を整数に変換
   private static int asInt(byte[] byte_array_4) {
     int ret = 0;
+    // 4バイトを結合して整数を構築
     for (int i = 0; i < 4; i++) {
       int b = (int) byte_array_4[i];
-      if (i < 3 && b < 0) {
+      if (i < 3 && b < 0) { // 負の値を補正
         b = 256 + b;
       }
-      ret += b << (i * 8);
+      ret += b << (i * 8); // 各バイトを適切な位置にシフトして加算
     }
-    return ret;
+    return ret; // 整数を返す
   }
 
+  // リトルエンディアン形式で入力ストリームから整数を読み取る
   public static int toIntLittleEndian(InputStream theInputStream) throws java.io.IOException {
     byte[] byte_array_4 = new byte[4];
-
+    // 4バイトをリトルエンディアン形式で読み込む
     byte_array_4[0] = (byte) theInputStream.read();
     byte_array_4[1] = (byte) theInputStream.read();
     byte_array_4[2] = (byte) theInputStream.read();
     byte_array_4[3] = (byte) theInputStream.read();
 
-    return asInt(byte_array_4);
+    return asInt(byte_array_4); // 読み取ったバイト配列を整数に変換
   }
 
+  // ビッグエンディアン形式で入力ストリームから整数を読み取る
   public static int toIntBigEndian(InputStream theInputStream) throws java.io.IOException {
     byte[] byte_array_4 = new byte[4];
-    /* used to reverse the int32 Big Endian of the tcp header to convert it to an int */
+    /* TCP ヘッダーの32ビット値（ビッグエンディアン形式）を整数に変換 */
     byte_array_4[3] = (byte) theInputStream.read();
     byte_array_4[2] = (byte) theInputStream.read();
     byte_array_4[1] = (byte) theInputStream.read();
     byte_array_4[0] = (byte) theInputStream.read();
-    return asInt(byte_array_4);
+    return asInt(byte_array_4); // 読み取ったバイト配列を整数に変換
   }
 
+  // 入力ストリームから長さを読み取り、その長さ分を文字列として返す
   public static String toString(InputStream ins) throws java.io.IOException {
-    int len = toIntLittleEndian(ins);
-    return toString(ins, len);
+    int len = toIntLittleEndian(ins); // 長さをリトルエンディアン形式で読み取る
+    return toString(ins, len); // 指定長のデータを文字列に変換
   }
 
+  // 入力ストリームから指定された長さ分を文字列として読み取る
   private static String toString(InputStream ins, int len) throws java.io.IOException {
     String ret = new String();
     for (int i = 0; i < len; i++) {
-      ret += (char) ins.read();
+      ret += (char) ins.read(); // 各バイトを文字として読み取る
     }
-    return ret;
+    return ret; // 文字列を返す
   }
 
+  // 整数をビッグエンディアン形式で出力ストリームに書き込む
   public static void toStream(OutputStream os, int i) throws Exception {
-    byte[] byte_array_4 = toByteArrayBigEndian(i);
-    os.write(byte_array_4);
+    byte[] byte_array_4 = toByteArrayBigEndian(i); // 整数をビッグエンディアン形式でバイト配列に変換
+    os.write(byte_array_4); // 出力ストリームに書き込む
   }
 
+  // 文字列を出力ストリームに書き込む
   public static void toStream(OutputStream os, String s) throws Exception {
     int len_s = s.length();
-    toStream(os, len_s);
+    toStream(os, len_s); // 文字列の長さを先に書き込む
     for (int i = 0; i < len_s; i++) {
-      os.write((byte) s.charAt(i));
+      os.write((byte) s.charAt(i)); // 文字列の各文字をバイトとして書き込む
     }
-    os.flush();
+    os.flush(); // バッファをフラッシュ
   }
 
+  // バイト配列を出力ストリームに書き込む
   public static void toStream(OutputStream os, byte[] theBytes) throws Exception {
     int myLength = theBytes.length;
-    toStream(os, myLength);
-    os.write(theBytes);
-    os.flush();
+    toStream(os, myLength); // バイト配列の長さを先に書き込む
+    os.write(theBytes); // バイト配列を書き込む
+    os.flush(); // バッファをフラッシュ
   }
 
+  // 入力ストリームからバイト配列を読み取る（長さ付き）
   public static byte[] toByteArray(InputStream ins) throws java.io.IOException {
-    int len = toIntLittleEndian(ins);
+    int len = toIntLittleEndian(ins); // 長さをリトルエンディアン形式で読み取る
     try {
-      return toByteArray(ins, len);
+      return toByteArray(ins, len); // 長さ分のバイトを読み取る
     } catch (Exception e) {
-      return new byte[0];
+      return new byte[0]; // 例外発生時は空の配列を返す
     }
   }
 
+  // 入力ストリームから指定された長さ分をバイト配列として読み取る
   protected static byte[] toByteArray(InputStream ins, int an_int)
       throws java.io.IOException, Exception {
 
-    byte[] ret = new byte[an_int];
+    byte[] ret = new byte[an_int]; // 指定された長さ分のバイト配列を作成
 
     int offset = 0;
     int numRead = 0;
     int outstanding = an_int;
 
     while ((offset < an_int) && ((numRead = ins.read(ret, offset, outstanding)) > 0)) {
-      offset += numRead;
-      outstanding = an_int - offset;
+      offset += numRead; // 読み取ったバイト数を更新
+      outstanding = an_int - offset; // 残りの読み取るバイト数
     }
     if (offset < ret.length) {
       throw new Exception(
           "Could not completely read from stream, numRead="
               + numRead
               + ", ret.length="
-              + ret.length); // ???
+              + ret.length); // ストリームから全てのデータを読み取れなかった場合
     }
-    return ret;
+    return ret; // 読み取ったバイト配列を返す
   }
 
+  // 入力ストリームからファイルに書き込む
   private static void toFile(InputStream ins, FileOutputStream fos, int len, int buf_size)
       throws java.io.FileNotFoundException, java.io.IOException {
 
@@ -337,43 +370,45 @@ public class Bytes {
     int total_len_read = 0;
 
     while (total_len_read + buf_size <= len) {
-      len_read = ins.read(buffer);
+      len_read = ins.read(buffer); // バッファサイズごとに読み込む
       total_len_read += len_read;
-      fos.write(buffer, 0, len_read);
+      fos.write(buffer, 0, len_read); // ファイルに書き込む
     }
 
     if (total_len_read < len) {
-      toFile(ins, fos, len - total_len_read, buf_size / 2);
+      toFile(ins, fos, len - total_len_read, buf_size / 2); // 残りのデータを再帰的に書き込む
     }
   }
 
+  // 入力ストリームからファイルに書き込む（指定長）
   private static void toFile(InputStream ins, File file, int len)
       throws java.io.FileNotFoundException, java.io.IOException {
 
-    FileOutputStream fos = new FileOutputStream(file);
+    FileOutputStream fos = new FileOutputStream(file); // 出力ファイルを開く
 
-    toFile(ins, fos, len, 1024);
+    toFile(ins, fos, len, 1024); // 1024バイトずつ書き込む
   }
 
+  // 入力ストリームからファイルに書き込む
   public static void toFile(InputStream ins, File file)
       throws java.io.FileNotFoundException, java.io.IOException {
 
-    int len = toIntLittleEndian(ins);
-    toFile(ins, file, len);
+    int len = toIntLittleEndian(ins); // 長さをリトルエンディアン形式で読み取る
+    toFile(ins, file, len); // ファイルに書き込む
   }
 
+  // 出力ストリームにファイルを転送する
   public static void toStream(OutputStream os, File file)
       throws java.io.FileNotFoundException, Exception {
 
-    toStream(os, (int) file.length());
-
+    toStream(os, (int) file.length()); // ファイルのサイズを転送
     byte b[] = new byte[1024];
-    InputStream is = new FileInputStream(file);
+    InputStream is = new FileInputStream(file); // ファイルを入力ストリームとして開く
     int numRead = 0;
 
     while ((numRead = is.read(b)) > 0) {
-      os.write(b, 0, numRead);
+      os.write(b, 0, numRead); // 出力ストリームに書き込む
     }
-    os.flush();
+    os.flush(); // バッファをフラッシュ
   }
 }
